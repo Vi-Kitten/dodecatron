08c0e6fb8f64da401daf724fb1acb39ace014a23
Vi <vi.kitten@proton.me>
2025-11-08 21:57:13 +0000

    starburst down

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,86 @@
-use std::ops::{Add, AddAssign, Mul, MulAssign, Neg};
+use std::{collections::HashSet, hash::Hash, ops::{Add, AddAssign, Mul, MulAssign, Neg, Not}};
+use enumerable::Enumerable;
 
 type Int = isize;
 type Nat = usize;
+
+trait CanMirror: Sized {
+    fn flip(self) -> Self;
+}
 
 trait Group: Mul<Self, Output = Self> + Sized {
     fn neutral() -> Self;
     fn inverse(self) -> Self;
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Enumerable)]
 struct Dihedral<G> {
     first_invert: bool,
     then_apply: G
 }
 
-#[derive(Debug, PartialEq, Eq)]
+impl<G> Dihedral<G> {
+    fn map<H>(self, f: impl FnOnce(G) -> H) -> Dihedral<H> {
+        Dihedral {
+            first_invert: self.first_invert,
+            then_apply: f(self.then_apply)
+        }
+    }
+}
+
+impl<G> From<G> for Dihedral<G> {
+    fn from(value: G) -> Self {
+        Dihedral {
+            first_invert: false,
+            then_apply: value
+        }
+    }
+}
+
+impl<G: Group> Mul<Dihedral<G>> for Dihedral<G> {
+    type Output = Dihedral<G>;
+
+    fn mul(self, rhs: Dihedral<G>) -> Self::Output {
+        if self.first_invert {
+            Dihedral {
+                first_invert: rhs.first_invert.not(),
+                then_apply: self.then_apply * rhs.then_apply.inverse()
+            }
+        } else {
+            Dihedral {
+                first_invert: rhs.first_invert,
+                then_apply: self.then_apply * rhs.then_apply }
+        }
+    }
+}
+
+impl<G: Group> Group for Dihedral<G> {
+    fn neutral() -> Self {
+        G::neutral().into()
+    }
+
+    fn inverse(self) -> Self {
+        if self.first_invert {
+            self
+        } else {
+            self.then_apply.inverse().into()
+        }
+    }
+}
+
+impl<T: CanMirror, G: Mul<T, Output = T>> Mul<T> for Dihedral<G> {
+    type Output = T;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        self.then_apply * if self.first_invert {
+            rhs.flip()
+        } else {
+            rhs
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Enumerable)]
 enum Cyclic6 {
     A0,
     A1,
@@ -105,31 +172,9 @@ impl Group for Cyclic6 {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct Dihedral6 {
-    first_flip: bool,
-    then_rotate: Cyclic6,
-}
+type Dihedral6 = Dihedral<Cyclic6>;
 
-impl Dihedral6 {
-    fn as_d12(self) -> Dihedral12 {
-        Dihedral12 {
-            first_flip: self.first_flip,
-            then_rotate: self.then_rotate.as_c12(),
-        }
-    }
-}
-
-impl From<Cyclic6> for Dihedral6 {
-    fn from(value: Cyclic6) -> Self {
-        Dihedral6 {
-            first_flip: false,
-            then_rotate: value,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Enumerable)]
 enum Cyclic12 {
     A0,
     A1,
@@ -334,20 +379,7 @@ impl Group for Cyclic12 {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct Dihedral12 {
-    first_flip: bool,
-    then_rotate: Cyclic12,
-}
-
-impl From<Cyclic12> for Dihedral12 {
-    fn from(value: Cyclic12) -> Self {
-        Dihedral12 {
-            first_flip: false,
-            then_rotate: value,
-        }
-    }
-}
+type Dihedral12 = Dihedral<Cyclic12>;
 
 #[derive(Debug, PartialEq, Eq)]
 enum Poly12Step {
@@ -492,29 +524,65 @@ impl Mul for Cyclotomic12 {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+fn reduce_by_symmetry<T: Clone + Eq + Hash, G: Enumerable + Group + Mul<T, Output = T>>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    let mut sym_items = HashSet::<T>::new();
+    iter.filter(move |t| {
+        for x in G::enumerator() {
+            if sym_items.contains(&(x * t.clone())) {
+                return false
+            }
+        }
+        sym_items.insert(t.clone());
+        true
+    })
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Star6([Nat; 6]);
+
+impl CanMirror for Star6 {
+    fn flip(self: Self) -> Self {
+        let Star6([x0, x1, x2, x3, x4, x5]) = self;
+        Star6([x0, x5, x4, x3, x2, x1])
+    }
+}
+
+impl Mul<Star6> for Cyclic6 {
+    type Output = Star6;
+
+    fn mul(self, Star6([x0, x1, x2, x3, x4, x5]): Star6) -> Self::Output {
+        match self {
+            Cyclic6::A0 => Star6([x0, x1, x2, x3, x4, x5]),
+            Cyclic6::A1 => Star6([x1, x2, x3, x4, x5, x0]),
+            Cyclic6::A2 => Star6([x2, x3, x4, x5, x0, x1]),
+            Cyclic6::A3 => Star6([x3, x4, x5, x0, x1, x2]),
+            Cyclic6::A4 => Star6([x4, x5, x0, x1, x2, x3]),
+            Cyclic6::A5 => Star6([x5, x0, x1, x2, x3, x4]),
+        }
+    }
+}
 
 impl Star6 {
     /// Generate all combinations of directions that add to zero (ignoring symmetry)
-    fn generate_raw_loops(n: Nat) -> Vec<Star6> {
-        // x  y  | z  | x' y' | z'
+    fn generate_raw_loops(n: Nat) -> Box<[Star6]> {
+        // matrix from star to Z adjoin 6th root of unity
+        // x  y  | u  | x' y' | u'
         // 1     | -1 | -1    | 1
         //    1  | 1  |    -1 | -1
 
         // dx = (x - x')
         // dy = (y - y')
-        // dz = (z - z')
+        // uz = (u - u')
 
-        // dx - dz = 0
-        // dy + dz = 0
-        // delta = dx = -dy = dz
+        // dx - du = 0
+        // dy + du = 0
+        // delta = dx = -dy = du
 
         // x_hat = min(x, x')
         // y_hat = min(y, y')
-        // z_hat = min(z, z')
+        // u_hat = min(u, u')
 
-        // a = x_hat + y_hat + z_hat
+        // a = x_hat + y_hat + u_hat
         // b = abs(delta)
 
         // n = 2a + 3b
@@ -524,23 +592,119 @@ impl Star6 {
         let mut a = (2 * n) % 3;
 
         loop {
-            let Some(b) = n.checked_sub(2 * 3).map(|m| m / 3) else {
-                return solutions;
+            let Some(b) = n.checked_sub(2 * a).map(|m| m / 3) else {
+                return solutions.into_boxed_slice();
             };
             for x_hat in 0..= a {
                 for y_hat in 0..= (a - x_hat) {
-                    let z_hat = a - x_hat - y_hat;
+                    let u_hat = a - x_hat - y_hat;
                     // delta = b
-                    solutions.push(Star6([x_hat + b, y_hat, z_hat + b, x_hat, y_hat + b, z_hat]));
+                    solutions.push(Star6([x_hat + b, y_hat, u_hat + b, x_hat, y_hat + b, u_hat]));
                     // delta = -b
-                    solutions.push(Star6([x_hat, y_hat + b, z_hat, x_hat + b, y_hat, z_hat + b]));
+                    solutions.push(Star6([x_hat, y_hat + b, u_hat, x_hat + b, y_hat, u_hat + b]));
                 }
             }
             a += 3
         }
     }
+
+    /// Generate all combinations of directions that add to zero
+    fn generate_loops(n: Nat) -> Box<[Star6]> {
+        reduce_by_symmetry::<Star6, Dihedral6>(Star6::generate_raw_loops(n).into_iter()).collect()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Star12([Nat; 12]);
+
+impl Star12 {
+    fn from_interspersed(
+        Star6([x0, x1, x2, x3, x4, x5]): Star6,
+        Star6([y0, y1, y2, y3, y4, y5]): Star6
+    ) -> Star12 {
+        Star12([x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5])
+    }
+}
+
+impl CanMirror for Star12 {
+    fn flip(self) -> Self {
+        let Star12([x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11]) = self;
+        Star12([x0, x11, x10, x9, x8, x7, x6, x5, x4, x3, x2, x1])
+    }
+}
+
+impl Mul<Star12> for Cyclic12 {
+    type Output = Star12;
+
+    fn mul(self, Star12([x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11]): Star12) -> Self::Output {
+        match self {
+            Cyclic12::A0 => Star12([x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11]),
+            Cyclic12::A1 => Star12([x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x0]),
+            Cyclic12::A2 => Star12([x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x0, x1]),
+            Cyclic12::A3 => Star12([x3, x4, x5, x6, x7, x8, x9, x10, x11, x0, x1, x2]),
+            Cyclic12::A4 => Star12([x4, x5, x6, x7, x8, x9, x10, x11, x0, x1, x2, x3]),
+            Cyclic12::A5 => Star12([x5, x6, x7, x8, x9, x10, x11, x0, x1, x2, x3, x4]),
+            Cyclic12::A6 => Star12([x6, x7, x8, x9, x10, x11, x0, x1, x2, x3, x4, x5]),
+            Cyclic12::A7 => Star12([x7, x8, x9, x10, x11, x0, x1, x2, x3, x4, x5, x6]),
+            Cyclic12::A8 => Star12([x8, x9, x10, x11, x0, x1, x2, x3, x4, x5, x6, x7]),
+            Cyclic12::A9 => Star12([x9, x10, x11, x0, x1, x2, x3, x4, x5, x6, x7, x8]),
+            Cyclic12::A10 => Star12([x10, x11, x0, x1, x2, x3, x4, x5, x6, x7, x8, x9]),
+            Cyclic12::A11 => Star12([x11, x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]),
+        }
+    }
+}
+
+impl Star12 {
+    /// Generate all combinations of directions that add to zero
+    fn generate_loops_upto(n: Nat) -> Box<[Box<[Star12]>]> {
+        // matrix from star to Z adjoin 12th root of unity
+        // x  y  z  w  | u' v' | x' y' z' w' | u' v'
+        // 1           | -1    | -1          | 1
+        //    1        |    -1 |    -1       |    1
+        //       1     | 1     |       -1    | -1
+        //          1  |    1  |          -1 |    -1
+
+        // can be seperated into
+        // x  y  | u  | x' y' | u'
+        // 1     | -1 | -1    | 1
+        //    1  | 1  |    -1 | -1
+        // and
+        // z  w  | v  | z' w' | v'
+        // 1     | -1 | -1    | 1
+        //    1  | 1  |    -1 | -1
+        // by interspersing both the inputs and outputs
+
+        let primary: Box<[Box<[Star6]>]> = (0..=n).map(Star6::generate_raw_loops).collect();
+        let secondary: Box<[Box<[Star6]>]> = primary.iter().map(|stars|
+            reduce_by_symmetry::<Star6, Dihedral6>(stars.iter().map(Star6::clone)).collect()
+        ).collect();
+        let mut bands: Box<[Vec<Star12>]> = (0..=n).map(|_| Vec::new()).collect();
+        for n_primary in 0..=n {
+            for n_secondary in 0..=(n - n_primary) {
+                let m = n_primary + n_secondary;
+                let primary_loops = &primary[n_primary];
+                let secondary_loops = &secondary[n_secondary];
+                primary_loops
+                    .iter()
+                    .map(|x| secondary_loops.iter().map(move |y| (x, y)))
+                    .flatten()
+                    .map(|(x, y)| Star12::from_interspersed(x.clone(), y.clone()))
+                    .for_each(|star| {
+                        bands[m].push(star)
+                    });
+            }
+        }
+        bands.into_iter().map(|band| reduce_by_symmetry::<Star12, Dihedral12>(band.into_iter()).collect()).collect()
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut n = 0;
+    for band in Star12::generate_loops_upto(6) {
+        println!("Stars of size {}", n);
+        for star in band {
+            println!("{:?}", star)
+        }
+        n += 1
+    }
 }
